3e52d4ce12f27b1d08ff2f3b9d179267004c095e
Ocklaf <ocklaf@gmail.com>
2023-07-24 18:41:02 +0200

    Exercise 5 Unit 2 OpenBootcamp

---
+++ src/CocheCRUD.java
@@ -0,0 +1,5 @@
+public interface CocheCRUD {
+    public void save();
+    public void findAll();
+    public void delete();
+}
---
+++ src/CocheCRUDImpl.java
@@ -0,0 +1,16 @@
+public class CocheCRUDImpl implements CocheCRUD{
+    @Override
+    public void save() {
+        System.out.println("Saving...");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("Finding all...");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("Deleting...");
+    }
+}
